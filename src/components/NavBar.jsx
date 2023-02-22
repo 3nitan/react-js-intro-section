f241cfa045d641473611aa1646b2b06c1ff625dd
Abimbola <mustaphaabimbola4@gmail.com>
2023-02-22 19:22:23 +0100

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import './NavBar.css';
+
+export default function NavBar() {
+  return (
+    <nav className='navbar'>
+      <div className='navbar-container'>
+        <ul className='navbar-items'>
+          <div className='logo'>
+            <a href='#'>
+              <img src='/assets/logo.svg' alt='logo' />
+            </a>
+          </div>
+          <li className='navbar-item'>
+            <a href='#' className='navbar-link'>
+              <p>Features</p>
+            </a>
+            <ul className='dropdown'>
+              <li>
+                <a href='#'>
+                  <p>
+                    <img src='/assets/icon-todo.svg' alt='todo icon' />
+                    Todo List
+                  </p>
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <p>
+                    <img src='/assets/icon-calendar.svg' alt='calendar logo' />
+                    Calendar
+                  </p>
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <p>
+                    <img src='/assets/icon-reminders.svg' alt='reminders' />
+                    Reminders
+                  </p>
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <p>
+                    <img src='/assets/icon-planning.svg' alt='planning' />
+                    Planning
+                  </p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li className='navbar-item'>
+            <a href='#' className='navbar-link'>
+              <p>Company</p>
+            </a>
+            <ul className='dropdown'>
+              <li>
+                <a href='#'>
+                  <p>History</p>
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <p>Our Team</p>
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <p>Blog</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li className='navbar-item'>
+            <a href='#' className='navbar-link'>
+              <p>Careers</p>
+            </a>
+          </li>
+
+          <li className='navbar-item'>
+            <a href='#' className='navbar-link'>
+              <p>About</p>
+            </a>
+          </li>
+        </ul>
+
+        <ul className='navbar-items'>
+          <li className='navbar-item'>
+            <a href='#' className='navbar-link'>
+              <p>Login</p>
+            </a>
+          </li>
+
+          <li className='navbar-item'>
+            <a href='#' className='navbar-link ouline'>
+              <p>Register</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
